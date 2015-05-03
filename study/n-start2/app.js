600506c7b021b553b5f89bbbcaae25dcb79617d2
yuna.lyn <yuna.lyn@alibaba-inc.com>
2015-05-03 12:02:37 +0800

    jade to xtpl

--- study/n-start2/app.js
+++ study/n-start2/app.js
@@ -7,9 +7,11 @@ var app = express();
 
 var routes = require('./router/index');
 
+
+
 app.set('views', path.join(__dirname, 'views'));
 
-app.set('view engine', 'jade');
+app.set('view engine', 'xtpl');
 
 app.use('/assets', express.static(__dirname + '/statics'));
 
@@ -20,6 +22,26 @@ app.use('/', routes);
 // 	res.send('hello world');
 // });
 
+// function test() {
+
+
+// 	console.log('process NODE_ENV', process.env.NODE_ENV);
+
+// 	app.set('env', 'production');
+
+// 	console.log(app.get('env'));
+
+// 	// 只用于开发环境
+// 	if ('development' == app.get('env')) {
+// 	  console.log('开发环境');
+// 	}
+
+// 	// 只用于生产环境
+// 	if ('production' == app.get('env')) {
+// 	  console.log('生产环境');
+// 	}
+// }
+// test();
 // 监听某个接口
 var server = app.listen(8181, function() {
 
--- study/n-start2/router/index.js
+++ study/n-start2/router/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-	res.render('index', {word: 'hello world!'});
+	res.render('index', {title: 'Hello'});
 });
 
 module.exports = router;
